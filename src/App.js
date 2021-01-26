d71480c31a14609842c1a860bc5dc54d2bd22122
genarene <genarene96@gmail.com>
2021-01-26 00:21:43 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Phone features={phoneApps} />
+    </>
   );
 }
+
+const phoneApps = [
+  {
+    id: 1,
+    subWoofer: "Subwoofer",
+    washable: "Non-porus Washable",
+    wings: "Wings",
+    beveledBezel: "Beveled Bezel",
+    seedless: "Seedless",
+  },
+];
+// main container
+const Phone = ({ features }) => {
+  return (
+    <>
+        {features.map((feature, index) => {
+          return (
+            <PhoneFeatures key={feature.id}>
+              <Subwoofer wooferText={feature.subWoofer} />
+              <Washable washableText={feature.washable} />
+              <Wings wingsText={feature.wings} />
+              <Beveled beveledText={feature.beveledBezel} />
+              <Beveled beveledText={feature.beveledBezel} />
+              <Seedless seedlessText={feature.seedless} />
+              <p>Add a card ...</p>
+           </PhoneFeatures>
+          );
+        })}
+      
+    </>
+  );
+};
+
+// component rendering the children
+const PhoneFeatures = ({ children }) => {
+  return <div className="phone-features">
+    <div><h3>Phone Features <span>...</span></h3></div>
+    {children}
+  </div>;
+};
+
+// subwoofer component
+const Subwoofer = ({ wooferText }) => {
+  return <div className="woofer">{wooferText}</div>;
+};
+
+// washable component
+const Washable = ({ washableText }) => {
+  return <div className="washable">{washableText}</div>;
+};
+
+// wings component
+const Wings = ({ wingsText }) => {
+  return <div className="wings">{wingsText}</div>;
+};
+
+// beveled component
+const Beveled = ({ beveledText }) => {
+  return <div className="beveled">{beveledText}</div>;
+};
+
+// seedless
+const Seedless = ({ seedlessText }) => {
+  return <div className="seedless">{seedlessText}</div>;
+};
 
 export default App;
